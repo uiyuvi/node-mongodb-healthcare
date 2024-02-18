c6116eff84f5bf9937167f029bddadd9ca5e0a73
yuvaraj <yuvaa.it@gmail.com>
2024-02-18 20:55:18 +0100

    Implement Add patient

--- API/addPatient.js
+++ API/addPatient.js
@@ -1,12 +1,25 @@
-let patientDetailsObj=require('../models/patients');
-let mongoose=require('mongoose');
+let patientDetailsObj = require('../models/patients');
+let mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('../config');
 const mongo = require('mongodb').MongoClient
 
-module.exports=function(req,res){
-    
-   /*write your code here*/
+module.exports = function (req, res) {
 
-  }
+   /*write your code here*/
+   console.log("add patient entry", req.body)
+
+
+   const newPatient = {
+      _id: new mongoose.Types.ObjectId(),
+      ...req.body
+   }
+   console.log('about to create patient', newPatient)
+   patientDetailsObj.create(newPatient, function (error, createdData) {
+      if (error) {
+         return res.status(400).json({ error: err });
+      }
+      return res.status(200).json({ status: "success" });
+   })
+}
