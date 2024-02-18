e5e86091963bb037eba15c353b6678ca1a5677af
yuvaraj <yuvaa.it@gmail.com>
2024-02-18 21:11:23 +0100

    Implement get disease

--- API/diseases.js
+++ API/diseases.js
@@ -1,11 +1,13 @@
-let diseases=require('../models/diseases');
-let mongoose=require('mongoose');
+let diseases = require('../models/diseases');
+let mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('../config');
 const mongo = require('mongodb').MongoClient
 
-module.exports=function(req,res){
+module.exports = async function (req, res) {
 
   /*write your code here*/
-  }
+  const patients = await diseases.find();
+  res.status(200).json(patients);
+}
