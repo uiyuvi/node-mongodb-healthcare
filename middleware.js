8e921afd66bf716fc693b90c7b76fab7fd7b74ae
yuvaraj <yuvaa.it@gmail.com>
2024-02-18 20:54:59 +0100

    Implement middleware

--- middleware.js
+++ middleware.js
@@ -3,9 +3,40 @@ const config = require('./config.js');
 
 let checkToken = (req, res, next) => {
 
-    /*write your middleware code here*/
-    
-	};
+  /*write your middleware code here*/
+  const secretToken = config.secret; // Secret token for generating JWT
+
+  // Extract the token from the request headers
+  const authHeader = req.header('Authorization') || req.header('x-access-token');
+  console.log("header", req.header)
+  if (!authHeader) {
+    console.log('token not valid')
+    return res.status(400).json({ error: 'Please authenticate' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Extract the token part
+
+  if (!token) {
+    console.log('token not found')
+    return res.status(400).json({ error: 'Please authenticate' });
+  }
+
+  try {
+    // Verify the token using the secret key
+    const decoded = jwt.verify(token, secretToken);
+    if (decoded._id.length > 0) {
+      console.log('token verified')
+      next();
+    } else {
+      console.log('token invalid', decoded)
+      return res.status(400).json({ error: 'Please authenticate' });
+    }
+  } catch (error) {
+    console.log('middleware error', error)
+    return res.status(400).json({ error: 'Please authenticate' });
+  }
+
+};
 
 
 module.exports = {
