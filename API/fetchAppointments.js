29c4096e6edbf6dba850540e7eae12b0f2bdbffd
yuvaraj <yuvaa.it@gmail.com>
2024-02-18 21:22:10 +0100

    Implement fetch appointment

--- API/fetchAppointments.js
+++ API/fetchAppointments.js
@@ -1,11 +1,13 @@
-let appointments=require('../models/appointments');
-let mongoose=require('mongoose');
+let appointments = require('../models/appointments');
+let mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('../config');
 const mongo = require('mongodb').MongoClient
 
-module.exports=function(req,res){
-    
+module.exports = async function (req, res) {
+
   /*write your code here*/
+  const appointmentsFound = await appointments.find();
+  res.status(200).json(appointmentsFound);
 }
