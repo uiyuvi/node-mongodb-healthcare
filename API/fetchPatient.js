f485af7d2e3b7147fba95201d65bbfeae3d3ae95
yuvaraj <yuvaa.it@gmail.com>
2024-02-18 21:09:55 +0100

    Implement fetch patient

--- API/fetchPatient.js
+++ API/fetchPatient.js
@@ -1,11 +1,13 @@
-let patientDetails=require('../models/patients');
-let mongoose=require('mongoose');
+let patientDetails = require('../models/patients');
+let mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('../config');
 const mongo = require('mongodb').MongoClient
 
-module.exports=function(req,res){
-    
-    /*write your code here*/
-  }
+module.exports = async function (req, res) {
+
+  /*write your code here*/
+  const patients = await patientDetails.find();
+  res.status(200).json(patients);
+}
